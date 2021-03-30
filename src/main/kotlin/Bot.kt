17fecb4af07c2cda1fe99a0cf474b4faa614b3ed
SoyNegro <krisoul.valdes@gmail.com>
2021-03-30 17:15:43 -0400

    Delete src/main/kotlin directory

--- src/main/kotlin/Bot.kt
+++
@@ -1,7 +0,0 @@
-import org.telegram.telegrambots.meta.TelegramBotsApi
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
-
-fun main(args: Array<String>){
- val telegramBotApi = TelegramBotsApi(DefaultBotSession::class.java)
-    telegramBotApi.registerBot(CryptoAlertBot())
-}
